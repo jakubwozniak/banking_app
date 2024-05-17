dbd0ef2496c9e2c8f24e3f8cc5262060c5b4c998
Jakub <kwozniak992@gmail.com>
2024-05-17 23:34:13 +0200

    fixed validation error on transaction transfer page

--- components/PaymentTransferForm.tsx
+++ components/PaymentTransferForm.tsx
@@ -27,7 +27,6 @@ import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 
 const formSchema = z.object({
-  email: z.string().email("Invalid email address"),
   name: z.string().min(4, "Transfer note is too short"),
   amount: z.string().min(4, "Amount is too short"),
   senderBank: z.string().min(4, "Please select a valid bank account"),
@@ -49,6 +48,7 @@ const PaymentTransferForm = ({ accounts }: PaymentTransferFormProps) => {
   });
 
   const submit = async (data: z.infer<typeof formSchema>) => {
+    console.log("submit");
     setIsLoading(true);
 
     try {
